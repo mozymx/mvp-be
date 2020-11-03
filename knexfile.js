3cc37a0c2acc3df95fc216bf4504e655ef45e52a
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-11-02 21:26:48 -0800

    Changes database to postgres

--- knexfile.js
+++ knexfile.js
@@ -1,65 +1,38 @@
+require("dotenv").config();
+
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/users.db3'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    searchPath: ['knex', 'public'],
     useNullAsDefault: true,
     migrations: {
       directory: "./database/migrations",
-      tableName: "dbmigrations",
     },
     seeds: {
-      directory: "./database/seeds"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on foreign key enforcement
-      },
+      directory: "./database/seeds/development"
     },
   },
   testing: {
-    client: "sqlite3",
-    connection: {
-      filename: "./database/test.db3",
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    searchPath: ['knex', 'public'],
     useNullAsDefault: true,
     migrations: {
       directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds/testing"
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    searchPath: ['knex', 'public'],
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./database/migrations"
     }
   }
 
