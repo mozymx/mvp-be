aa888b9a2c1e7b6cba8ac77fcec16eaf58b0b963
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-11-08 21:43:25 -0800

    Adds '/bank-accounts' and '/delete-account' endpoints

--- banks/banks-model.js
+++ banks/banks-model.js
@@ -2,9 +2,27 @@ const db = require("../database/dbConfig");
 
 module.exports = {
     addBank,
+    getAllBanks,
+    getBankByID,
+    deleteBank
 }
 
 // add bank for a customer
 function addBank(customerData) {
     return db("bank").insert(customerData).returning("id");
+}
+
+// get all banks
+function getAllBanks() {
+    return db("bank");
+}
+
+// get bank by certain filter
+function getBankByID(bankID) {
+    return db("bank").where({ id: bankID }).first();
+}
+
+// delete bank for a customer
+function deleteBank(bankID) {
+    return db("bank").where({ id: bankID }).del();
 }
--- banks/banks-router.js
+++ banks/banks-router.js
@@ -18,9 +18,20 @@ router.get("/belvo-access-token", (req, res) => {
             res.json(response);
         })
         .catch((error) => {
-            res.status(500).send({ message: error.message });
+            res.status(500).json({ error });
         });
     });
+})
+
+// get all bank account links
+router.get("/bank-accounts", (req, res) => {
+    Banks.getAllBanks()
+    .then((bankAccounts) => {
+        res.status(200).json({ bankAccounts });
+    })
+    .catch((error) => {
+        res.status(500).json({ error })
+    })
 })
 
 // create link to bank account
@@ -47,13 +58,46 @@ router.post("/register-account/:customerID", (req, res) => {
                 res.status(201).end();
             })
             .catch((error) => {
-                res.status(500).send({ error });
+                res.status(500).json({ error });
             })
         })
         .catch((error) => {
-            res.status(500).send({ error });
+            res.status(500).json({ error });
         });
     });
+})
+
+// delete a specific bank account
+router.delete("/delete-account/:bankID", (req, res) => {
+    const bankID = req.params.bankID;
+
+    Banks.getBankByID(bankID)
+    .then((bankAccount) => {
+        // first delete from our database
+        Banks.deleteBank(bankAccount.id)
+        .then((response) => {
+            // then from Belvo API
+            client.connect()
+            .then(() => {
+                client.links.delete(bankAccount.account)
+                .then((response) => {
+                    res.status(204).end();
+                })
+                .catch((error) => {
+                    res.status(500).json({ error });
+                })
+            })
+        })
+    })
+})
+
+router.get("/:bankID", (req, res) => {
+    const bankID = req.params.bankID;
+
+    Banks.getBankByID(bankID)
+    .then((response) => {
+        console.log("RESPONSE:", response);
+    })
 })
 
 module.exports = router;
