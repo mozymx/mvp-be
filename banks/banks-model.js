77d94ce71a5c8d3f52074d118e4c7727c8c1bf06
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-11-18 17:21:37 -0800

    Adds '/save-bank-account/' and '/account-details/' routes

--- banks/banks-model.js
+++ banks/banks-model.js
@@ -1,28 +1,28 @@
 const db = require("../database/dbConfig");
 
 module.exports = {
-    addBank,
-    getAllBanks,
-    getBankByID,
-    deleteBank
-}
+  addBank,
+  getAllBanks,
+  getBankByID,
+  deleteBank,
+};
 
 // add bank for a customer
 function addBank(customerData) {
-    return db("bank").insert(customerData).returning("id");
+  return db("bank").insert(customerData).returning("id");
 }
 
 // get all banks
 function getAllBanks() {
-    return db("bank");
+  return db("bank");
 }
 
-// get bank by certain filter
+// get bank by filter
 function getBankByID(bankID) {
-    return db("bank").where({ id: bankID }).first();
+  return db("bank").where({ id: bankID }).first();
 }
 
 // delete bank for a customer
 function deleteBank(bankID) {
-    return db("bank").where({ id: bankID }).del();
+  return db("bank").where({ id: bankID }).del();
 }
