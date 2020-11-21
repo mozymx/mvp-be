edc42c6df882f94de18d0b9224805894713780a7
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-11-20 21:33:57 -0800

    Migrate 'available-banks' from banks to belvo router

--- banks/banks-router.js
+++ banks/banks-router.js
@@ -10,30 +10,6 @@ const client = new belvo(
   process.env.BELVO_URL
 );
 
-// get all available banks from Belvo
-router.get("/available-banks", (req, res) => {
-  client.connect().then(() => {
-    client.institutions
-      .list()
-      .then((institutions) => {
-        const mexicoRetailBanks = institutions.filter((institution) => {
-          if (
-            institution.type === "bank" &&
-            institution["country_code"] === "MX" &&
-            institution.name.includes("mx_retail")
-          ) {
-            return institution;
-          }
-        });
-        res.status(200).json(mexicoRetailBanks);
-      })
-      .catch((error) => {
-        console.log("ERROR", error);
-        res.status(401).json({ error });
-      });
-  });
-});
-
 /* BANK CONNECTIONS (Belvo Links) */
 
 // connect user to bank through Belvo
--- belvo/belvo-router.js
+++ belvo/belvo-router.js
@@ -7,8 +7,28 @@ const client = new belvo(
   process.env.BELVO_URL
 );
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Belvo router working..." });
+// get all available banks from Belvo
+router.get("/available-banks", (req, res) => {
+  client.connect().then(() => {
+    client.institutions
+      .list()
+      .then((institutions) => {
+        const mexicoRetailBanks = institutions.filter((institution) => {
+          if (
+            institution.type === "bank" &&
+            institution["country_code"] === "MX" &&
+            institution.name.includes("mx_retail")
+          ) {
+            return institution;
+          }
+        });
+        res.status(200).json(mexicoRetailBanks);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+        res.status(401).json({ error });
+      });
+  });
 });
 
 module.exports = router;
